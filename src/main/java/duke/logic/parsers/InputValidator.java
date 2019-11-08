9dee1167baf31ac58bdfcf2f2be7e69156086b33
Ivan Andika <ivanandika@u.nus.edu>
2019-11-08 20:27:57 +0800

    add validateAmount to check whether amount is numeric

--- src/main/java/duke/logic/parsers/InputValidator.java
+++ src/main/java/duke/logic/parsers/InputValidator.java
@@ -2,6 +2,9 @@ package duke.logic.parsers;
 
 import duke.commons.exceptions.DukeException;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
 /**
  * InputValidator is a public class that deals with validating user input.
  */
@@ -16,6 +19,17 @@ public class InputValidator {
         if (userInput.trim().length() == 0) {
             throw new DukeException("OOPS!!! The description of the command cannot be empty.");
         }
-        //TODO: Throw exceptions for different commands
     }
+
+    /**
+     * validate the amount the user input (nutritional value or food cost) is numeric.
+     * @param userInput String input by user.
+     * @throws DukeException If the userInput is not numeric.
+     */
+    public static void validateAmount(String userInput) throws DukeException {
+        if (!userInput.matches("-?\\d+(\\.\\d+)?")) {
+            throw new DukeException("The nutritional value or food cost must be numeric.");
+        }
+    }
+
 }
